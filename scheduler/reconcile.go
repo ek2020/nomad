72023e75b916f4061272a087ae46f82c1560dbe0
Juanadelacuesta <juanita.delacuestamorales@hashicorp.com>
2023-10-13 13:31:29 +0200

    style: change how the error is handled, for clarity, no functional changes

--- scheduler/reconcile.go
+++ scheduler/reconcile.go
@@ -1358,8 +1358,8 @@ func (a *allocReconciler) createTimeoutLaterEvals(disconnecting allocSet, tgName
 	}
 
 	timeoutDelays, err := disconnecting.delayByMaxClientDisconnect(a.now)
-	if err != nil || len(timeoutDelays) != len(disconnecting) {
-		a.logger.Error("error computing disconnecting timeouts for task_group",
+	if err != nil {
+		a.logger.Error("error for task_group",
 			"task_group", tgName, "error", err)
 		return map[string]string{}
 	}
